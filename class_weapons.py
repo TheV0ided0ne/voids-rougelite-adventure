017b9ca19fe13dca1af63bcbcf57e28ec9ec5405
Firephinxs <jakefarr246@gmail.com>
2026-04-30 21:46:59 +1000

    done the brawler

--- class_weapons.py
+++ class_weapons.py
@@ -1,10 +1,10 @@
 class Weapons:
-    def __init__(self,strength, damage, luck, constitution,Dexterity):
+    def __init__(self,strength, damage, luck, constitution,dexterity):
         self.strength = strength
         self.damage = damage
         self.constitution = constitution
         self.luck = luck
-        self.dexterity = Dexterity
+        self.dexterity = dexterity
 
 
 class StarterDagger(Weapons):
@@ -15,4 +15,13 @@ class StarterDagger(Weapons):
             luck=2,
             constitution=0,
             dexterity=2
+        )
+class starterFists(Weapons):
+    def __init__(self):
+        super().__init__(
+            strength=2,
+            constitution=2,
+            damage=3,
+            luck=0,
+            dexterity=0
         )
