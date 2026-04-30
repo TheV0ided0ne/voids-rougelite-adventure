aeb4956c567c5ef188a5363cf4a3894b383993a2
TheV0ided0ne <chrislastguest@gmail.com>
2026-04-30 14:23:31 +1000

    Void's Rougelite Adventure!

--- forest.py
+++ forest.py
@@ -2,3 +2,22 @@ import classes
 import enemies
 import inventory
 import main
+
+print("You start within the forest of damnation, a mythical land where ramage is common, and you are always in danger.")
+print("What shall you do?")
+
+print("[1] Scavenge")
+print("[2] Hunt")
+print("[3] Get a grasp of your surroundings")
+
+
+
+forest1 = input("> ").strip().lower()
+if forest1 == "1":
+    print("Scavenge")
+
+if forest1 == "2":
+    print("Hunt")
+
+if forest1 == "3":
+    print("Get a grasp")
--- main.py
+++ main.py
@@ -35,22 +35,7 @@ if choice in class_map:
 print("Game started!")
 print("If you need to use a certain command but forgot how to do it, do 'help' in the terminal")
 
-print("You start within the forest of damnation, a mythical land where ramage is common, and you are always in danger.")
-print("What shall you do?")
 
-print("[1] Scavenge")
-print("[2] Hunt")
-print("[3] Get a grasp of your surroundings")
-
-forest1 = input("> ").strip().lower()
-if forest1 == "1":
-    print("Scavenge")
-
-if forest1 == "2":
-    print("Hunt")
-
-if forest1 == "3":
-    print("Get a grasp")
 
 while True:
     command = input("\n> ").strip().lower()
