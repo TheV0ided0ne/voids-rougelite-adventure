5c1a7d36d77d66c2ffe97276c12d6b3a13f4685f
TheV0ided0ne <chrislastguest@gmail.com>
2026-05-07 13:36:40 +1000

    started working more on stats

--- statistics.py
+++ statistics.py
@@ -7,7 +7,6 @@ class Statistics:
                  constitution, dexterity, intelligence,
                  wisdom, charisma):
 
-        self.damage = damage
         self.strength = strength
         self.dexterity = dexterity
         self.constitution = constitution
@@ -18,7 +17,6 @@ class Statistics:
 
     def __repr__(self):
         return (f"{self.__class__.__name__}"
-                f" | Damage: {self.damage} | "
                 f"Dex: {self.dexterity} | Con: {self.constitution} | "
                 f"Int: {self.intelligence} | Wis: {self.wisdom} | "
                 f"Cha: {self.charisma} | Luc: {self.luck} | ")
